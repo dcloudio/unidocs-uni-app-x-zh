3ca1a67416faa0289d9e84d3afc48cee5c5b9936
DCloud_LXH <283700113@qq.com>
2023-12-11 17:27:41 +0800

    feat: create sitemap

--- docs/.vuepress/markdown/createSiteMap.js
+++ docs/.vuepress/markdown/createSiteMap.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const domain = 'https://zh.uniapp.dcloud.io'
+const domain = 'https://doc.dcloud.net.cn'
 const xmlBefore = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 `
@@ -9,6 +9,7 @@ const xmlAfter = `\n</urlset>`
 
 module.exports = (links, callback = () => { }) => {
   const xmlItems = links.map(url => {
+    if (!url.endsWith('/') && !url.endsWith('html')) url += '.html'
     return `  <url>
     <loc>${domain + url}</loc>
   </url>`
