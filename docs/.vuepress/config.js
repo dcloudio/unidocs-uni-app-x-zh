4531d9965acf32ca3779dbb45baf4079315cbf72
DCloud_LXH <283700113@qq.com>
2023-12-13 17:11:59 +0800

    fix: docsRepo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -48,7 +48,7 @@ const config = {
     nextLinks: false,
     prevLinks: false,
     repo: 'dcloudio/uni-app',
-    docsRepo: 'https://gitcode.net/dcloud/unidocs-uni-app-x',
+    docsRepo: 'https://gitcode.net/dcloud/unidocs-uni-app-x-zh',
     docsBranch: 'master',
     docsDir: 'docs',
     editLinks: true,
