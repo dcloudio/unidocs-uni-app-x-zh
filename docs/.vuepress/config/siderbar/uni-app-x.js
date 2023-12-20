4ebe895f7ca0d8cc2f1dc4bd3f3820bbaa1fe02b
DCloud_LXH <283700113@qq.com>
2023-12-20 15:28:04 +0800

    chore: update theme to 1.4.1

--- docs/.vuepress/config/siderbar/uni-app-x.js
+++ docs/.vuepress/config/siderbar/uni-app-x.js
@@ -1,42 +1,66 @@
 export default {
-	contactItems: [
+	contacts: [
 		{
-			name: 'uniCloud Web控制台',
-			url: 'https://unicloud.dcloud.net.cn',
-			imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/cloud@2x.jpg',
+			type: 'link',
+			items: [
+				{
+					name: 'uniCloud Web控制台',
+					url: 'https://unicloud.dcloud.net.cn',
+					imageUrl:
+						'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/cloud@2x.jpg',
+				},
+				{
+					name: '插件市场',
+					url: 'https://ext.dcloud.net.cn/',
+					imageUrl:
+						'https://img-cdn-aliyun.dcloud.net.cn/dev/img/ext/favicon.png',
+				},
+				{
+					name: 'ask问答社区',
+					url: 'https://ask.dcloud.net.cn/explore/',
+					imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/d@2x.png',
+				},
+				{
+					name: 'uniAD',
+					url: 'https://uniad.dcloud.net.cn',
+					imageUrl:
+						'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uniad-520.png',
+				},
+				{
+					name: 'uni统计',
+					url: 'https://tongji.dcloud.net.cn/',
+					imageUrl:
+						'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-tongji-icon.png',
+				},
+			],
 		},
 		{
-			name: '插件市场',
-			url: 'https://ext.dcloud.net.cn/',
-			imageUrl: 'https://img-cdn-aliyun.dcloud.net.cn/dev/img/ext/favicon.png',
+			type: 'links',
+			items: [
+				{
+					name: 'hello uni-app x',
+					url: 'https://gitcode.net/dcloud/hello-uni-app-x',
+				},
+				{
+					name: 'uni-api',
+					url: 'https://gitcode.net/dcloud/uni-api',
+				},
+				{
+					name: 'uni-component',
+					url: 'https://gitcode.net/dcloud/uni-component',
+				},
+			],
 		},
 		{
-			name: 'ask问答社区',
-			url: 'https://ask.dcloud.net.cn/explore/',
-			imageUrl: 'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/d@2x.png',
+			type: 'link',
+			items: [
+				{
+					name: 'uni-im交流群',
+					imageUrl:
+						'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/chat-icon-r.png',
+					url: 'https://im.dcloud.net.cn/#/?joinGroup=658160eb3e585b555acebfd7',
+				},
+			],
 		},
-		{
-			name: 'uniAD',
-			url: 'https://uniad.dcloud.net.cn',
-			imageUrl:
-				'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uniad-520.png',
-		},
-		{
-			name: 'uni统计',
-			url: 'https://tongji.dcloud.net.cn/',
-			imageUrl:
-				'https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/uni-tongji-icon.png',
-		},
-	],
-	codeHosting: [
-		{
-			name: '码云',
-			url: 'https://gitee.com/dcloud/uni-app',
-		},
-		{
-			name: 'GitHub',
-			url: 'http://github.com/dcloudio/uni-app',
-		},
-	],
-	qq_group: [],
+	]
 };
