2cb3b6ac7922c2e30a83a9049b42db706ab7e5b6
DCloud_LXH <283700113@qq.com>
2025-05-10 18:47:38 +0800

    fix: img-add-attrs

--- docs/.vuepress/markdown/img-add-attrs.js
+++ docs/.vuepress/markdown/img-add-attrs.js
@@ -12,27 +12,36 @@ module.exports = function (md, opts) {
     let joinTokenContent = false
     let handleToken = null
     for (let i = 0; i < state.tokens.length; i++) {
-      const token = state.tokens[i]
-      if (token.type === 'html_block') {
+      const blockToken = state.tokens[i]
+      if (blockToken.type === 'html_block') {
         if (joinTokenContent && handleToken) {
-          handleToken.content += token.content
+          handleToken.content += blockToken.content
           state.tokens.splice(i, 1)
           i--
         } else {
           if (i === state.tokens.length - 1) {
             // 如果是最后一个token，直接替换
-            replaceHTML(token, addLoadingAttr, state.env)
+            replaceHTML(blockToken, addLoadingAttr, state.env)
           } else {
             joinTokenContent = true
-            handleToken = token
+            handleToken = blockToken
           }
         }
       } else {
         joinTokenContent = false
         if (handleToken) {
           replaceHTML(handleToken, addLoadingAttr, state.env)
+          handleToken = null
         }
-        handleToken = null
+
+        if (blockToken.type === 'inline' && blockToken.children) {
+          blockToken.children.forEach(function (token) {
+            const type = token.type
+            if (type === 'html_inline') {
+              replaceHTML(token, addLoadingAttr, state.env)
+            }
+          })
+        }
       }
     }
     return false
@@ -55,7 +64,7 @@ function addLoadingAttr(attribs) {
  * @returns
  */
 function matchSrc(src) {
-  return typeof src === 'string' ? src.indexOf('qiniu-web-assets.dcloud.net.cn') > -1 || src.indexOf('web-ext-storage.dcloud.net.cn') > -1 : false
+  return typeof src === 'string' && ['qiniu-web-assets.dcloud.net.cn','web-ext-storage.dcloud.net.cn'].some((item) => src.includes(item))
 }
 
 function replaceNodes(nodes, replace, env, token) {
@@ -76,9 +85,14 @@ function replaceHTML(token, replace, env) {
   const serializer = require('dom-serializer')
   const dom = new htmlparser.parseDocument(token.content, {
     lowerCaseTags: false,
-    recognizeCDATA: true,
-    recognizeSelfClosing: true,
+    xmlMode: true,
+    decodeEntities: false,
   })
+  /**
+   * 会将 <a/> 标签解析成 <a></a>
+   * 会将 <style></style> 标签解析
+   */
+  if (!dom.firstChild || ['a', 'style'].includes(dom.firstChild.name)) return
   replaceNodes(dom.children, replace, env, token)
-  token.content = serializer.render(dom, { encodeEntities: 'utf8', xmlMode: 'foreign' })
+  token.content = serializer.render(dom, { encodeEntities: false, xmlMode: 'foreign' })
 }
